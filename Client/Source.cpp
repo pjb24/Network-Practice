615fa6dd72e1b610329bf1e168be367acfb1663b
박재복 <pjb24@daum.net>
2022-05-16 15:25:43 +0900

    WinSocket 생성, 제거 추가.

--- Client/Source.cpp
+++ Client/Source.cpp
@@ -1,8 +1,18 @@
 //Client Code
+//Author: Jacob Preston 2019-04-04
+
 #include <PNet/IncludeMe.h>
+#include <iostream>
+
+using namespace PNet;
 
 int main()
 {
-	int value = PNet::ReturnFive();
+	if ( Network::Initialize() )
+	{
+		std::cout << "Winsock api successfully initialized." << std::endl;
+	}
+	Network::Shutdown();
+
 	return 0;
 }
--- Server/Source.cpp
+++ Server/Source.cpp
@@ -1,8 +1,19 @@
 //Server Code
+//Author: Jacob Preston 2019-04-04
+
 #include <PNet/IncludeMe.h>
+#include <iostream>
+
+using namespace PNet;
 
 int main()
 {
-	int value = PNet::ReturnFive();
+	if ( Network::Initialize() )
+	{
+		std::cout << "Winsock api successfully initialized." << std::endl;
+	}
+	Network::Shutdown();
+
+	system( "pause" );
 	return 0;
 }
