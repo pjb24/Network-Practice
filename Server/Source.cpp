d63a9b4adda2bd1c41348756b2fe24b97aab60c6
박재복 <pjb24@daum.net>
2022-05-16 15:43:06 +0900

    Socket Create, Close 추가.

--- Server/Source.cpp
+++ Server/Source.cpp
@@ -11,6 +11,16 @@ int main()
 	if ( Network::Initialize() )
 	{
 		std::cout << "Winsock api successfully initialized." << std::endl;
+		Socket socket;
+		if ( socket.Create() == PResult::P_Success )
+		{
+			std::cout << "Socket successfully created." << std::endl;
+			socket.Close();
+		}
+		else
+		{
+			std::cout << "Socket failed to create." << std::endl;
+		}
 	}
 	Network::Shutdown();
 
