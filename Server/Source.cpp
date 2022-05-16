653728c503fd0c7820ecb696c104c276263facd0
박재복 <pjb24@daum.net>
2022-05-16 16:13:39 +0900

    "192.168.0.1", port 형식으로 입력된 데이터를 분해하고 저장하는 IPEndpoint 클래스 추가.

--- Server/Source.cpp
+++ Server/Source.cpp
@@ -11,6 +11,24 @@ int main()
 	if ( Network::Initialize() )
 	{
 		std::cout << "Winsock api successfully initialized." << std::endl;
+
+		IPEndpoint test( "192.168.0.2", 8080 );
+		if ( test.GetIPVersion() == IPVersion::IPv4 )
+		{
+			std::cout << "Hostname: " << test.GetHostname() << std::endl;
+			std::cout << "IP: " << test.GetIPString() << std::endl;
+			std::cout << "Port: " << test.GetPort() << std::endl;
+			std::cout << "IP Bytes... " << std::endl;
+			for ( auto& digit : test.GetIPBytes() )
+			{
+				std::cout << (int)digit << std::endl;
+			}
+		}
+		else
+		{
+			std::cerr << "This is not an ipv4 address." << std::endl;
+		}
+
 		Socket socket;
 		if ( socket.Create() == PResult::P_Success )
 		{
